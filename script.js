74ecc62722507bec9faf9ae43b41a22e56fdcb4c
matthewheston <matthewheston@gmail.com>
2013-09-17 18:23:26 -0500

    Modify Array.prototype with map and filter if native implementations don't exist.

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// if we have native map and filter, use them, otherwise update Array.prototype
+if (!Array.prototype.map) {
+  Array.prototype.map = function map(func, array) {
+    var result = [];
+    forEach(array, function (element) {
+      result.push(func(element));
+    });
+    return result;
+  }
+}
+// Array.prototype.filter imeplemetationcCopyright (c) Mozilla Foundation
+// http://www.mozilla.org/ This code is available under the terms of the
+// MIT License.
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun /*, thisp*/) {
+    var len = this.length >>> 0;
+    if (typeof fun != "function") {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++) {
+      if (i in this) {
+        var val = this[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, this)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}
+
 $(function() {
   var crimes = getData();
   appendDates(crimes);
